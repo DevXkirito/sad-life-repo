8a671b9f4ec240bb51ec8c4d747ac8f56f042293
DevXkirito <111516421+DevXkirito@users.noreply.github.com>
2023-06-26 22:50:00 +0530

    Update alive.py

--- YorForger/modules/alive.py
+++ YorForger/modules/alive.py
@@ -1,18 +1,19 @@
-import os
-import re
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
 from platform import python_version as kontol
 from telethon import events, Button
 from telegram import __version__ as telever
 from telethon import __version__ as tlhver
 from pyrogram import __version__ as pyrover
 from YorForger.events import register
-from YorForger import telethn as tbot
+
 
 
 PHOTO = "https://telegra.ph/file/ff2fa22dfa6ae838cc6cd.jpg"
 
-@register(pattern=("/alive"))
-async def awake(event):
+@app.on_message(filters.command("alive"))
+async def awake(client, message):
   TEXT = f"**ʜᴇʏ, ɪ ᴀᴍ ₮₳₦JłⱤØӾ₭₳₦₳Ø.** \n\n"
   TEXT += f"**➖➖➖➖➖➖➖➖➖**"
   TEXT += f"**» ᴍʏ ᴏᴡɴᴇʀ : [Kirito 『 𓃠 』](https://t.me/Kirito_est)** \n\n"
@@ -20,5 +21,11 @@ async def awake(event):
   TEXT += f"**» ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{tlhver}` \n\n"
   TEXT += f"**»ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{pyrover}` \n\n"
   TEXT += f"**➖➖➖➖➖➖➖➖➖**"
-  BUTTON = [[Button.url("Update", "https://t.me/TeamxXYZ"), Button.url("Support", "https://t.me/MitsuriHelpSupport")]]
-  await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
+ 
+BUTTON = InlineKeyboardMarkup(
+    [[
+        InlineKeyboardButton("Update", url="https://t.me/TeamxXYZ"),
+        InlineKeyboardButton("Support", url="https://t.me/MitsuriHelpSupport")
+    ]]
+)
+    await client.send_photo(message.chat.id, PHOTO, caption=TEXT, reply_markup=BUTTON)
